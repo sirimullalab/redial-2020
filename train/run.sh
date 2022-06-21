bf190acbbe1c48ff88b9bcd91426546b75be966f
Govinda KC <govinda.kc@roivant.com>
2022-06-21 16:49:15 -0700

    Update run.sh

--- train/run.sh
+++ train/run.sh
@@ -1,1 +1,2 @@
+# Example: generate features for a sample csv file. Make sure ../new_features dir is created
 python3 features_generation.py --csvfile ./sample_data_stand.csv --model CPE --split-type te --ft ecfp0 --numpy_folder ../new_features
